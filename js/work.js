556c4213ade12af863dc96a5979b085a01be7538
Austin Arnett <ajarnett94@gmail.com>
2017-04-22 19:33:03 -0400

    basic movement & start on gamepad

--- js/work.js
+++ js/work.js
@@ -6,11 +6,28 @@ function preload() {
 	game.load.image('Bullet', 'img/bullet.png');
 }
 
+var CB1
+var CB2
+var bullet
+
 function create() {
-	game.add.sprite(0, 0, 'CB1');
-	game.add.sprite(40, 0, 'CB2');
-	game.add.sprite(30, 0, 'Bullet');
+	CB1 = game.add.sprite(0, 0, 'CB1');
+	CB2 = game.add.sprite(40, 0, 'CB2');
+	bullet = game.add.sprite(30, 0, 'Bullet');
+	
+	cursors = game.input.keyboard.createCursorKeys();
+	
+	game.physics.arcade.enable(CB1);
 }
 
 function update() {
+	CB1.body.velocity.y = 0;
+    CB1.body.velocity.x = 0;
+
+	if (cursors.up.isDown) { CB1.body.velocity.y = -150; }
+	else if (cursors.down.isDown) {CB1.body.velocity.y = 150; }
+    else if (cursors.left.isDown) { CB1.body.velocity.x = -150; }
+    else if (cursors.right.isDown) { CB1.body.velocity.x = 150; }
+    else {}
+	
 }
