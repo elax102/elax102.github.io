b4aacb09818e508ab288835672fedd4a2c996730
jacob green <jacob green>
2017-04-22 14:45:01 -0400

    sprite add

--- js/work.js
+++ js/work.js
@@ -1,9 +1,15 @@
 var game = new Phaser.Game(512, 512, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+	game.load.image('CB1', 'img/cowboy1.png');
+	game.load.image('CB2', 'img/cowboy2.png');
+	game.load.image('Bullet', 'img/bullet.png');
 }
 
 function create() {
+	game.add.sprite(0, 0, 'CB1');
+	game.add.sprite(40, 0, 'CB2');
+	game.add.sprite(30, 0, 'Bullet');
 }
 
 function update() {
