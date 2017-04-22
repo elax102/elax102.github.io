8955fc1d1686c71d8f585a602efdc8395b885ff3
Alex Davis <davis.alex.jr@gmail.com>
2017-04-22 14:06:27 -0400

    Init commit

---
+++ js/work.js
@@ -0,0 +1,10 @@
+var game = new Phaser.Game(512, 512, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
