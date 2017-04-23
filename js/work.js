d0ae1247fc4a4c5898f12c873481d4c679f15e64
Austin Arnett <ajarnett94@gmail.com>
2017-04-23 09:44:44 -0400

    second gamepad addition

--- js/work.js
+++ js/work.js
@@ -42,35 +42,33 @@ function create() {
     bullets.setAll('outOfBoundsKill', true);
 	//bullets.body.allowRotation = true;
 	
+	//================ Cowboy 1 =====================
+	
 	CB1 = game.add.sprite(100, 100, 'CB1');
 	CB1.anchor.set(0.5, 0.5);
-	//CB1.rotation = 0.5;
+	
 	game.physics.arcade.enable(CB1, Phaser.Physics.ARCADE);
 	CB1Pad = game.input.gamepad.pad1;
 	
 	CB1.body.collideWorldBounds = true;
-	
 
-    CB1.body.allowRotation = true;
-	
+	CB1.body.setCircle(30);
+	//================ Cowboy 2 =====================
 	CB2 = game.add.sprite(300, 300, 'CB2');
 	CB2.anchor.set(0.5, 0.5);
-	//CB2.rotation = 0.5;
+	
 	game.physics.arcade.enable(CB2, Phaser.Physics.ARCADE);
-	//CB2Pad = game.input.gamepad.pad2;
+	CB2Pad = game.input.gamepad.pad2;
 	
 	CB2.body.collideWorldBounds = true;
 	
-
-    CB2.body.allowRotation = true;
-	 CB1.body.setCircle(30);
     CB2.body.setCircle(30);
 }
 
 function update() {
 	//set Cowboy speed to zero at start of update
     CB1.body.velocity.x = 0; CB1.body.velocity.y = 0;
-    //CB2.body.velocity.x = 0; CB2.body.velocity.y = 0;
+    CB2.body.velocity.x = 0; CB2.body.velocity.y = 0;
 	
 	//temporary movement with keyboard
 	if (cursors.up.isDown) { CB1.body.velocity.y = -150; }
@@ -78,7 +76,11 @@ function update() {
     else if (cursors.left.isDown) { CB1.body.velocity.x = -150; }
     else if (cursors.right.isDown) { CB1.body.velocity.x = 150; }
     else {}
+	
+	//Mouse
+	//CB1.rotation = fixRotation(game.physics.arcade.angleToPointer(CB1));
 
+	//============== Cowboy 1 Gamepad =================
 	CB1leftStickX = CB1Pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X);
 	CB1leftStickY = CB1Pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y);
 	if (CB1leftStickX < -DEADZONE || CB1leftStickX > DEADZONE) {
@@ -91,11 +93,22 @@ function update() {
 	CB1rightStickX = CB1Pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
 	CB1rightStickY = CB1Pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
 	
-	//Gamepad
 	CB1.angle = fixRotation(Math.atan2(CB1rightStickY, CB1rightStickX)) * (180/Math.PI);
 	
-	//Mouse
-	//CB1.rotation = fixRotation(game.physics.arcade.angleToPointer(CB1));
+	//============== Cowboy 2 Gamepad =================
+	CB2leftStickX = CB2Pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X);
+	CB2leftStickY = CB2Pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y);
+	if (CB2leftStickX < -DEADZONE || CB2leftStickX > DEADZONE) {
+        CB2.body.velocity.x = SPEED * CB2leftStickX;
+    }
+    if (CB2leftStickY < -DEADZONE || CB2leftStickY > DEADZONE) {
+        CB2.body.velocity.y = SPEED * CB2leftStickY;
+    }
+	
+	CB2rightStickX = CB2Pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+	CB2rightStickY = CB2Pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+	
+	CB2.angle = fixRotation(Math.atan2(CB2rightStickY, CB2rightStickX)) * (180/Math.PI);
 	
 	game.physics.arcade.collide(bullets, CB2, hitHandler, null, this);
 	game.physics.arcade.collide(ammo, CB1, pickHandler, null, this);
