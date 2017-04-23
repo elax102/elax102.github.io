af7a0bb78c4f5265544e3a0ba9d94f70148523b9
jacob green <jacob green>
2017-04-22 21:17:03 -0400

    shoot pickup

--- js/states/preload.js
+++ js/states/preload.js
@@ -1,5 +1,6 @@
-export default class Preload extends Phaser.State {game.load.image('CB1', 'img/cowboy1.png');
-	preload() {game.load.image('CB2', 'img/cowboy2.png');
+export default class Preload extends Phaser.State {
+	preload() {game.load.image('CB1', 'img/cowboy1.png');
+		game.load.image('CB2', 'img/cowboy2.png');
 	game.load.image('Bullet', 'img/bullet.png');
 	game.load.image('background', 'img/bg1-tiled.png');
 game.load.image('rock', 'img/rock1.png');}
